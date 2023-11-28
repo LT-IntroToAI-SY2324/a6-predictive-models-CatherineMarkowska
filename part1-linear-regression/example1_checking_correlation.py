2d73b23bcca3a5efaa5e9191ed3938f83c4fd998
Catherine Markowska <cimarkowska@cps.edu>
2023-11-28 10:37:34 -0600

    worked on a6 and writeup

--- part1-linear-regression/example1_checking_correlation.py
+++ part1-linear-regression/example1_checking_correlation.py
@@ -3,9 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # gets the data and sets x and y values
-data = pd.read_csv("chirping_data.csv")
+data = pd.read_csv("part1-linear-regression/chirping_data.csv")
+print(data)
 x = data["Temp"]
 y = data["Chirps"]
+print(x)
 
 # sets the size of the graph
 plt.figure(figsize=(6,4))
