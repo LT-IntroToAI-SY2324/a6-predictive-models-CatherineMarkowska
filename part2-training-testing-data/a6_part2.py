861415c283e739e064e3a5a62a10379f04a464e9
Catherine Markowska <cimarkowska@cps.edu>
2023-12-01 10:36:27 -0600

    worked on a6 part 2

--- part2-training-testing-data/a6_part2.py
+++ part2-training-testing-data/a6_part2.py
@@ -13,6 +13,13 @@ x = data["Age"].values
 y = data["Blood Pressure"].values
 
 # Create your training and testing datasets:
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = .2)
+print(f"x {x}")
+print(f"y {y}")
+print(f"xtrain {xtrain}")
+print(f"xtest {xtest}")
+print(f"ytrain {ytrain}")
+print(f"ytest {ytest}")
 
 # Use reshape to turn the x values into 2D arrays:
 xtrain = xtrain.reshape(-1,1)
@@ -27,26 +34,46 @@ intercept = round(float(model.intercept_), 2)
 r_squared = model.score(x, y)
 
 # Print out the linear equation and r squared value:
-print(f"Model's Linear Equation: y = {coef}x + {intercept}")
-print(f"R Squared value: {r_squared}")
+print("Model's Linear Equation: y=",coef, "x+", intercept)
+print("R Squared value:", r_squared)
 
 '''
 **********TEST THE MODEL**********
 '''
 # reshape the xtest data into a 2D array
-
+xtest = xtest.reshape(-1,1)
 
 # get the predicted y values for the xtest values - returns an array of the results
+predict = model.predict(xtest)
 
 # round the value in the np array to 2 decimal places
 predict = np.around(predict, 2)
-print(predict )
 
 
 # Test the model by looping through all of the values in the xtest dataset
 print("\nTesting Linear Model with Testing Data:")
-
+for index in range(len(xtest)):
+    actual = ytest[index] # gets the actual y value from the ytest dataset
+    predicted_y = predict[index] # gets the predicted y value from the predict variable
+    x_coord = xtest[index] # gets the x value from the xtest dataset
+    print("x value:", float(x_coord), "Predicted y value:", predicted_y, "Actual y value:", actual)
 
 '''
 **********CREATE A VISUAL OF THE RESULTS**********
 '''
+#sets the size of the graph
+plt.figure(figsize=(5,4))
+
+#creates a scatter plot and labels the axes
+plt.scatter(xtrain,ytrain, c="purple", label="Training Data")
+plt.scatter(xtest, ytest, c="green", label="Testing Data")
+
+plt.scatter(xtest, predict, c="red", label="Predictions")
+
+plt.xlabel("Age (Years)")
+plt.ylabel("Blood Pressure (mmHG)")
+plt.title("Age by Blood Pressure")
+plt.plot(x, coef*x + intercept, c="r", label="Line of Best Fit")
+
+plt.legend()
+plt.show()
