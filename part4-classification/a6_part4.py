96040abf6c4c3a8d110334918ec4a9d42f423a3e
Catherine Markowska <cimarkowska@cps.edu>
2023-12-07 10:38:39 -0600

    worked on a6 part 4

--- part4-classification/a6_part4.py
+++ part4-classification/a6_part4.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import pandas as pd
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -10,12 +12,17 @@ x = data[["Age", "EstimatedSalary", "Gender"]].values
 y = data["Purchased"].values
 
 # Step 1: Print the values for x and y
+print(x)
+print(y)
 
 # Step 2: Standardize the data using StandardScaler, 
+scaler = StandardScaler().fit(x)
 
 # Step 3: Transform the data
+x = scaler.transform(x)
 
 # Step 4: Split the data into training and testing data
+x_train, x_test, y_train, y_test = train_test_split(x, y) 
 
 # Step 5: Fit the data
 
