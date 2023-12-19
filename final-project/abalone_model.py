466aacc00d0d37eb8cc1bdb21e89565aa1274338
Catherine Markowska <cimarkowska@cps.edu>
2023-12-19 08:09:41 -0600

    added abalone data

--- final-project/abalone_model.py
+++ final-project/abalone_model.py
@@ -1,20 +1,7 @@
-pip install ucimlrepo
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
 
-from ucimlrepo import fetch_ucirepo 
-  
-# fetch dataset 
-abalone = fetch_ucirepo(id=1) 
-  
-# data (as pandas dataframes) 
-X = abalone.data.features 
-y = abalone.data.targets 
-  
-# metadata 
-print(abalone.metadata) 
-  
-# variable information 
-print(abalone.variables) 
+data = pd.read_csv("a6-predictive-models/abalone_data.csv")
+
 
